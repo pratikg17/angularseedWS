267002feb1ddab8a996ab62721889bd9e2efcbb6
Pratik <pratikg17@github.com>
2016-12-10 23:43:51 +0530

    d

--- app/templates/templates.js
+++ app/templates/templates.js
@@ -15,12 +15,25 @@ angular.module('templateStore.templates',['ngRoute'])
 .controller('TemplatesCtrl',['$scope', '$http',function($scope,$http){
 	 
 	 $http.get('json/templates.json').success(function(data){
-	 	//console.log(data);
-
 	 	$scope.templates=data;
 	 	console.log(data.name);
 	 });
 }])
-.controller('TemplatesDetailsCtrl',['$scope','$routeParams',function($scope ,$routeParams){
-  	console.log("Params",$routeParams);
+.controller('TemplatesDetailsCtrl',['$scope','$routeParams','$http','$filter',function($scope ,$routeParams,$http,$filter){
+	 var templateId =$routeParams.templateId;
+
+	 $http.get('json/templates.json').success(function(data){
+	 	$scope.template=$filter('filter')(data,function(d){
+	 		return d.id=templateId;
+	 	})[0];
+	 	$scope.mainImage =$scope.template.images[4].name;
+	 	 console.log("IMAGE",$scope.mainImage);
+
+	 });
+
+	 $scope.setImage =function(image){
+	 	$scope.mainImage=image.name;
+	 }
+
+
 }]);
